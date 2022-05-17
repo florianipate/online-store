efe5690f8db9129d46713dc2ec70c802fa3ea5cf
Florian Ipate <florianipate@Florians-iMac.local>
2022-05-17 09:27:29 +0100

    app updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-
+ 
 
 const App = () => {
+  console.log('render');
+  const categories =[
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Sneakers'
+    },
+    {
+      id: 4,
+      title: 'Womens'
+    },
+    {
+      id: 5,
+      title: 'Mens'
+    }
+  ]
   return (
     <div className="vrs_catergories-container">
-      <div className="vrs_category-container">
+      {categories.map(({title, id}) => (
+        <div className="vrs_category-container">
         {/* <img/> */}
-        <div className="vrs_category-body-container">
-          <h2>Hats</h2>
+        <div className="vrs_category-body-container" key={id}>
+          <h2>{title}</h2>
           <p>Shop Now</p>
         </div>
       </div>
+      ))}
+      
     </div>
   );
 }
