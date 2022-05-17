179330340cf22da3ece7b34ebfc4d014ebc03b99
Florian Ipate <florian.ipate@gmail.com>
2022-05-17 19:05:22 +0100

    update directory

--- src/components/vrs_directory/vrs_directory.component.jsx
+++ src/components/vrs_directory/vrs_directory.component.jsx
@@ -1,6 +1,8 @@
+import CategoryItem from '../vrs_category-item/vrs_category_item.component';
+import './vrs_directory.style.scss';
 const Directory =() =>{
     return (
-        <div className="categories-container">
+        <div className="directory-container">
             {categories.map((category) => (
                 <CategoryItem  key={category.id} category = {category} />
             ))}
