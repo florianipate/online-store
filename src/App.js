e44604d3467dfae47355dabe55bcd3976dbb5b16
Florian Ipate <florianipate@Florians-iMac.local>
2022-05-17 12:52:17 +0100

    Added Sass file

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
- 
+ import './categories.styles.scss';
 
 const App = () => {
   console.log('render');
   const categories =[
     {
-      id: 1,
-      title: 'Hats'
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      id: 2,
-      title: 'Jackets'
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      id: 3,
-      title: 'Sneakers'
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      id: 4,
-      title: 'Womens'
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      id: 5,
-      title: 'Mens'
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
     }
   ]
+  
   return (
-    <div className="vrs_catergories-container">
-      {categories.map(({title, id}) => (
-        <div className="vrs_category-container">
-        <div className="vrs_background-image"></div>
-        <div className="vrs_category-body-container" key={id}>
+    <div className="categories-container">
+      {categories.map(({title, id, imageUrl}) => (
+        <div className="category-container" key={id}>
+        <div className="background-image"
+        style={{
+          backgroundImage:`url(${imageUrl})`,
+        }}
+        />
+        <div className="category-body-container" >
           <h2>{title}</h2>
           <p>Shop Now</p>
         </div>
