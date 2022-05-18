e82390bb8139adab880234583479f04af8ff8c62
Florian Ipate <florian.ipate@gmail.com>
2022-05-18 20:25:25 +0100

    update routers

--- src/App.js
+++ src/App.js
@@ -1,38 +1,5 @@
- import Directory from "./components/vrs_directory/vrs_directory.component";
-
+ import Home from "./routes/home/vrs_home.component"; 
 const App = () => {
-  console.log('render');
-  const categories =[
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  
-  return (
-    <Directory categories ={categories} />
-  );
+ return <Home />;
 }
-
 export default App;
